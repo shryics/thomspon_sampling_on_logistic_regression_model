bc25c40f57a370ca8773bbdfd06802542a974042
tlab <tlab@sd.tmu.ac.jp>
2019-10-25 19:24:31 +0900

    fix: class

--- Thompson_Sampling_on_Logistic_Regression_Model.py
+++ Thompson_Sampling_on_Logistic_Regression_Model.py
@@ -15,57 +15,66 @@ class TSonLR:
         self.max_val   = -9999
         self.max_index = -9999
 
-    def Hesse(self, data, t):
+    def Hesse(self, data, T):
         e = math.e
-        sum = 0
-        for i in range(t+1):
-            a = np.array(data.iloc[i])
+        sum = np.eye(self.dim) / self.sigma
+        for t in range(T):
+            a = np.array(data.iloc[t])
             sum += (e ** (np.dot(self.theta.T, a))) * (np.dot(np.matrix(a).T, np.matrix(a))) / (1 + e ** (np.dot(self.theta.T, a))) ** 2
         return sum
 
-    def Gradient(self, data, t):
+    def Gradient(self, data, T, rewards):
+        def X(a, reward):
+            return a if reward else 0
         e = math.e
-        sum = 0
-        for i in range(t+1):
-            a = np.array(data.iloc[i])
-            sum += (e ** (np.dot(self.theta.T, a))) * (a) / (1 + e ** (np.dot(self.theta.T, a)))
+        sum = self.theta / self.sigma
+        for t in range(T):
+            a = np.array(data.iloc[t])
+            sum += (e ** (np.dot(self.theta.T, a))) * (a) / (1 + e ** (np.dot(self.theta.T, a))) - X(a, rewards[t])
         return sum
 
-    def update_theta(self, data, t):
+    def update_theta(self, data, T, rewards):
         while True:
-            self.H_t = np.eye(self.dim) / self.sigma + self.Hesse(data, t)
-            self.G_t = self.theta / self.sigma + self.Gradient(data, t)
+            self.H_t = self.Hesse(data, T)
+            self.G_t = self.Gradient(data, T, rewards)
             theta_tmp = self.theta - np.dot(np.linalg.inv(self.H_t), self.G_t)
             theta_tmp = np.ravel(theta_tmp)
             if np.allclose(self.theta, theta_tmp):
                 break
             self.theta = theta_tmp
 
-    def predict(self, a_t):
+    def predict(self, a):
         theta_nami = np.random.multivariate_normal(self.theta, np.linalg.inv(self.H_t))
-        for i in range(len(a_t)):
-            product = np.dot(a_t[i].T, theta_nami)
-            if self.max_val < product:
-                self.max_val = product
-                self.max_index = i
+        product = np.dot(a.T, theta_nami)
+        if self.max_val < product:
+            self.max_val = product
+            self.max_index = i
         return self.max_index
 
 
 
-data = pd.read_csv("data/data.txt", sep="\t").iloc[:,:5]
+data = pd.read_csv("data/data.txt", sep="\t").iloc[:,:]
 sigma = 0.5 ** 2
 dim = 5
 theta = np.array([0 for i in range(dim)])
+rewards = [0]
 
 tslr = TSonLR(sigma=sigma, dim=dim)
 
-for t in range(0, 144):
-    a_t = [0, 0, 0]
-    a_t[0] = np.array([random.uniform(0, 100) for i in range(dim)])
-    a_t[1] = np.array([random.uniform(50, 150) for i in range(dim)])
-    a_t[2] = np.array([random.uniform(100, 200) for i in range(dim)])
+for t in range(1, 144):
+    a = data.iloc[:t, :5]
+    y = data.iloc[:t, 10]
+    print(a, y)
 
-    tslr.update_theta(data, t)
-    max_index = tslr.predict(a_t)
+    a_actual = data.iloc[t, :5]
+    y_actual = data.iloc[t, 10]
 
-    print(t, max_index)
+    tslr.update_theta(a, t, rewards)
+    y_pred = tslr.predict(a_actual)
+
+    if pred == actual:
+        rewards.append(1)
+    else:
+        rewards.append(0)
+
+    print(t, pred)
